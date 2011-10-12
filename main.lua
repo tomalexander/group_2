10f6aadb99ebead1e749b9676e2cee810ee30da6
Colin Neville <nevilc@rpi.edu>
2011-10-11 22:09:25 -0400

    Background and HUD now follow along with the view

--- main.lua
+++ main.lua
@@ -113,6 +113,11 @@ function onFrame(event)
 		end
 	end
 	
+	background.x = viewx + 960/2
+	--background_ground0
+	--background_ground1
+	hud.group.x = viewx
+	
 	--[[
 	if test_goright then
 		move_screen_right(1)
@@ -225,7 +230,17 @@ function move_screen_left(amount)
 end
 
 function move_screen(amount)
-	display.getCurrentStage():translate(-amount, 0)
+	local stage = display.getCurrentStage()
+	stage:translate(-amount, 0)
+	
+	--[[
+	for i = 0, stage.numChildren do
+		if stage[i] then
+			--stage[i]:translate(amount, 0)
+		end
+	end
+	--]]
+	
 	ground.scroll(viewx + amount, viewx)
 	viewx = viewx + amount
 end
